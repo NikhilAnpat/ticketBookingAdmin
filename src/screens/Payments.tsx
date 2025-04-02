cf78d1d6bbcbec7204b845c82d88714464891a82
KantaJadhav <jadhavsayli501@gmail.com>
2025-04-02 10:05:34 +0530

    payment screen done (#15)
    Co-authored-by: ajaysinhap <ajaysinhpawar2@gmail.com>

--- src/screens/Payments.tsx
+++ src/screens/Payments.tsx
@@ -2,11 +2,12 @@
 import { useState, useRef, useEffect } from "react";
 import { DetailsList, DetailsListLayoutMode, IColumn } from "@fluentui/react";
 import { Button } from "@fluentui/react-components";
-import { Search, Filter,Calendar,  } from 'lucide-react';
+import { Search, Filter, Calendar, } from 'lucide-react';
 import { DateRange, RangeKeyDict, Range } from 'react-date-range';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
-import { FaRegEdit } from "react-icons/fa";
+
+import { LuSquareArrowOutUpRight } from "react-icons/lu";
 import bookingsData from "../components/dummyData/payments/payments.json";
 interface Booking {
   id: string;
@@ -29,8 +30,8 @@ const columns: IColumn[] = [
     maxWidth: 250,
     flexGrow: 1,
     onRender: (item: Booking) => (
-      <div className="flex items-center gap-2"  style={{ fontSize: "16px" }}>
-        
+      <div className="flex items-center gap-2" style={{ fontSize: "16px" }}>
+
         {item.name}
       </div>
     ),
@@ -65,7 +66,7 @@ const columns: IColumn[] = [
     flexGrow: 1,
     onRender: (item: Booking) => {
       let statusClass = "text-white rounded-xl px-2 py-1 text-xs";
-    
+
       if (item.status === "Confirmed") {
         statusClass += " bg-green-500";
       } else if (item.status === "Pending") {
@@ -73,7 +74,7 @@ const columns: IColumn[] = [
       } else if (item.status === "Cancelled") {
         statusClass += " bg-red-500 px-2.5";
       }
-    
+
       return <button className={statusClass}>{item.status}</button>;
     },
   },
@@ -84,7 +85,7 @@ const columns: IColumn[] = [
     maxWidth: 120,
     flexGrow: 1,
     headerClassName: "text-lg font-bold text-gray-800",
-    onRender: () => <Button  icon={<FaRegEdit />} title="Edit" />,
+    onRender: () => <Button icon={<LuSquareArrowOutUpRight />} title="View" />,
   },
 ];
 const Payments = () => {
@@ -103,23 +104,23 @@ const Payments = () => {
   ]);
   const [searchQuery, setSearchQuery] = useState<string>("");
   const dropdownRef = useRef<HTMLDivElement>(null);
-  
-    const toggleDropdown = () => {
-      setIsOpen((prev) => !prev);
-    };
-  
-    useEffect(() => {
-      function handleClickOutside(event: MouseEvent) {
-        if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-          setIsOpen(false);
-        }
+
+  const toggleDropdown = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
       }
-  
-      document.addEventListener('mousedown', handleClickOutside);
-      return () => {
-        document.removeEventListener('mousedown', handleClickOutside);
-      };
-    }, []);
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
 
   useEffect(() => {
@@ -127,7 +128,7 @@ const Payments = () => {
   }, []);
 
   const filteredBookings = bookings.filter((booking) => {
-   
+
     const matchesStatus = selectedStatus ? booking.status === selectedStatus : true;
     const normalizedSearchQuery = searchQuery.replace(/\s+/g, "");
     const matchesSearchQuery =
@@ -140,31 +141,32 @@ const Payments = () => {
   });
   return (
     <div className="p-5 w-full">
-      <div className=" rounded-lg bg-white p-2  overflow-hidden">
-        <div className="flex flex-row justify-between p-2">
-          <h4 className="font-semibold text-xl">Transactions</h4>
-          <div className="flex flex-row justify-center gap-3" >
-            <div className="relative">
-              <Search className="w-5 h-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+      <div className="rounded-lg bg-white p-2 ">
+        <div className="flex flex-wrap sm:flex-row justify-between p-2">
+          <h4 className="font-semibold text-xl w-full sm:w-auto">Transactions</h4>
+          <div className="flex flex-wrap justify-center gap-3 w-full sm:w-auto">
+            <div className="relative w-full sm:w-64">
+              <Search className="w-5 h-5 absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400" />
               <input
                 type="text"
-                placeholder="Search name,airline etc."
-                className="pl-10 pr-4 py-2 border rounded-lg w-64 focus:outline-none focus:ring-2 focus:ring-amber-500"
-                value={searchQuery} // bind the search input to searchQuery
+                placeholder="Search name, airline etc."
+                className="pl-10 pr-4 py-2 border rounded-lg w-full sm:w-64 focus:outline-none focus:ring-2 focus:ring-amber-500"
+                value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
               />
             </div>
+
             {/* Status Filter */}
-            <div className="relative z-20  " ref={statusRef}>
+            <div className="relative z-50 w-full sm:w-auto" ref={statusRef}>
               <button
-                className="border rounded-lg px-3 w-32 py-3 sm:text-[15px] text-xs flex items-center space-x-2 hover:bg-gray-50 "
+                className="border rounded-lg px-3 py-3 sm:text-[15px] text-xs w-full sm:w-32 flex items-center space-x-2 hover:bg-gray-50"
                 onClick={() => setShowStatusDropdown(!showStatusDropdown)}
               >
                 <Filter className="w-4 h-4" />
-                <span >{selectedStatus || "Status"}</span>
+                <span>{selectedStatus || "Status"}</span>
               </button>
               {showStatusDropdown && (
-                <div className="absolute z-10 mt-2 bg-white border rounded-lg shadow-lg w-[120px]">
+                <div className="absolute  mt-2 bg-white border rounded-lg shadow-lg w-full sm:w-[120px]">
                   <button
                     className="w-full px-3 py-2 text-left hover:bg-gray-100"
                     onClick={() => {
@@ -177,7 +179,7 @@ const Payments = () => {
                   {statusOptions.map((status) => (
                     <button
                       key={status}
-                      className=" px-3 py-2 text-left hover:bg-gray-100"
+                      className="px-3 py-2 text-left hover:bg-gray-100"
                       onClick={() => {
                         setSelectedStatus(status);
                         setShowStatusDropdown(false);
@@ -189,23 +191,24 @@ const Payments = () => {
                 </div>
               )}
             </div>
-            <div className="relative " ref={dropdownRef}>
+
+            {/* Date Range Picker */}
+            <div className="relative z-50 w-full sm:w-auto " ref={dropdownRef}>
               <button
                 onClick={toggleDropdown}
-                className="flex cursor-pointer items-center gap-2 text-[14px] w-40 border rounded-lg px-2 py-2 bg-[#E4C779] shadow-md text-gray-700"
+                className="flex cursor-pointer items-center gap-2 text-[14px] w-full sm:w-40 border rounded-lg px-2 py-2 bg-[#E4C779] shadow-md text-gray-700"
               >
                 <Calendar size={24} />
-                <span >
-                {range[0].startDate && range[0].endDate ? (
-                  range[0].startDate.toDateString() === range[0].endDate.toDateString()
-                    ? new Intl.DateTimeFormat('en-GB', { day: 'numeric', month: 'long', year: 'numeric' }).format(range[0].startDate)
-                    : `${range[0].startDate.getDate()}-${range[0].endDate.getDate()} ${range[0].endDate.toLocaleString('en-GB', { month: 'long' })} ${range[0].endDate.getFullYear()}`
-                ) : 'Select Date'}
+                <span>
+                  {range[0].startDate && range[0].endDate ? (
+                    range[0].startDate.toDateString() === range[0].endDate.toDateString()
+                      ? new Intl.DateTimeFormat('en-GB', { day: 'numeric', month: 'long', year: 'numeric' }).format(range[0].startDate)
+                      : `${range[0].startDate.getDate()}-${range[0].endDate.getDate()} ${range[0].endDate.toLocaleString('en-GB', { month: 'long' })} ${range[0].endDate.getFullYear()}`
+                  ) : 'Select Date'}
                 </span>
-                
               </button>
               {isOpen && (
-                <div className="absolute z-10 mt-2 bg-white border rounded-lg shadow-lg right-0">
+                <div className="absolute mt-2  bg-white border rounded-lg shadow-lg right-0">
                   <DateRange
                     editableDateInputs={false}
                     onChange={(item: RangeKeyDict) => setRange([item.selection])}
@@ -218,6 +221,8 @@ const Payments = () => {
             </div>
           </div>
         </div>
+
+
 
         <div className="h-[400px] overflow-y-auto rounded-xl">
 
@@ -232,13 +237,14 @@ const Payments = () => {
               headerWrapper: {
                 position: "sticky",
                 top: 0,
-                zIndex: 10,
+                zIndex: 5,
                 background: "white",
               },
             }}
           />
         </div>
       </div>
-    </div>)
+    </div>
+  )
 }
 export default Payments
